bd67efdee6990b7c6bb394ecce80668bda5c3d9b
StephanRammos <techeagle289@gmail.com>
2025-06-26 15:28:24 -0700

    Initial commit

---
+++ ch2/2-6.py
@@ -0,0 +1,3 @@
+famous_person = "Constantin Theodory"
+message = "I will be glad if I have succeeded in impressing the idea that it is not only pleasant to read\nat times the works of the old mathematical authors, but this may occasionally be of use for the actual advancement of science."
+print(f'{famous_person} said {message}')
