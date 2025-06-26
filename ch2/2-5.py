88b7ca906c1109eef2fdf04003438e84ae2502f1
StephanRammos <techeagle289@gmail.com>
2025-06-26 15:22:53 -0700

    Update 2-5.py with slight changes

--- ch2/2-5.py
+++ ch2/2-5.py
@@ -1,2 +1,2 @@
 famous_person = "Constantin Theodory"
-print(f'{famous_person} said "I will be glad if I have succeeded in impressing the idea that it is not only pleasant to read\n at times the works of the old mathematical authors, but this may occasionally be of use for the actual advancement of science"')
+print(f'{famous_person} said "I will be glad if I have succeeded in impressing the idea that it is not only pleasant to read\nat times the works of the old mathematical authors, but this may occasionally be of use for the actual advancement of science"')
