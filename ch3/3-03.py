25177a562691189f02447920072c2d290cbc1e15
StephanRammos <techeagle289@gmail.com>
2025-06-27 15:28:34 -0700

    Initial commit

---
+++ ch3/3-03.py
@@ -0,0 +1,11 @@
+#Mult - line strings
+speed_utility = "Alaskan XL by Wooldridge" 
+inf_fish = "15ft Saturn Heavy-Duty Fishing Boat HD470"
+message = (
+	f"My favorite mode of transport is by boat\n"
+
+	f"My favorite utility speedboat is the {speed_utility}\n"
+	f"My favorite inflatable fishing boat is {inf_fish}"					
+
+	)
+print(message)
