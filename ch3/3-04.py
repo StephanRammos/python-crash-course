bc1f860fdbf3443cd67548218a877a27d84fd403
StephanRammos <techeagle289@gmail.com>
2025-06-27 15:48:26 -0700

    Initial commit

---
+++ ch3/3-04.py
@@ -0,0 +1,5 @@
+# 3-04 Guest list - print a message to each guest
+guests = ['Chris','Haylee','Kelsea']
+message =  " has been invited to dinner with the stars event hosted by S"
+for i in range(0, len(guests)):
+	print(guests[i] + message)
