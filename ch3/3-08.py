7c62687afab97300977b2a21f5cc6154523c1425
StephanRammos <techeagle289@gmail.com>
2025-06-27 21:03:50 -0700

    added sorted function with reverse parameter

--- ch3/3-08.py
+++ ch3/3-08.py
@@ -1,2 +1,10 @@
 #3-08.py  8 travel locations
-locations = ['Niihau', 'Kauai', 'Oahu', 'Molokai', 'Lanai', 'Kahoolawe', 'Maui', 'Hawaii']										'Oahu', ]
+locations = ['Niihau', 'Kauai', 'Oahu', 'Molokai', 'Lanai', 'Kahoolawe', 'Maui', 'Hawaii']	
+# .sort method returns None bc it overwrites the original list, unlike the sorted() function which returns a new list leaving original unchanged
+
+print(sorted(locations))
+#sh ow original list is unchanged 
+print(locations)
+#Use sorted() to print your list in reverse alphabetical order
+print(sorted(locations, reverse = 'True'))
+
