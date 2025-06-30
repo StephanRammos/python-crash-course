e4cf1a0d1ff7e3f4c767e4dd3417c522bb27c137
StephanRammos <techeagle289@gmail.com>
2025-06-30 14:45:37 -0700

    Initial commit

---
+++ ch4/4-02.py
@@ -0,0 +1,16 @@
+# 4-02.py
+quadrupeds = ["dog", "cat", "elephant"] 
+for quadruped in quadrupeds:
+	print(quadruped)
+
+print("______________")
+
+for quadruped in quadrupeds:
+	print(f"A {quadruped} would make a great pet")
+
+print("____________________")
+
+for quadruped in quadrupeds:
+	print(f"A {quadruped} would make a great pet")
+
+print(f"These animals: {quadrupeds[0]}, {quadrupeds[1]} and {quadrupeds[2]}; all use 4 legs for locomotion")
