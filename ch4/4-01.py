4faab36efb644bd2af6fd14a6487c564cfd2d30b
StephanRammos <techeagle289@gmail.com>
2025-06-30 11:45:44 -0700

    Add ch4 exercises

---
+++ ch4/4-01.py
@@ -0,0 +1,7 @@
+# 4-01.py 
+pizzas = ['Hawaiian', 'Sicilian', 'White cheese']
+for pizza in pizzas:
+	print(f"I like {pizza} pizza")
+
+#Outside for loop. Print stmt of all pizzas i like
+print(f"I like {pizzas[0]}, {pizzas[1]} and {pizzas[2]} pizzas. \nPizza is great")
