2f972c91cc95bfe783f439e46305c559bc9803ee
StephanRammos <techeagle289@gmail.com>
2025-07-01 12:44:19 -0700

    initial

---
+++ ch4/4-12.py
@@ -0,0 +1,10 @@
+#4-12.py
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friends_foods = my_foods[:]
+friends_foods.append('canoli')
+
+for my_food in my_foods:
+	print(my_food)
+print("---------------\n")
+for friends_food in friends_foods:
+	print(friends_food)
