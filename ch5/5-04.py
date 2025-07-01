8061827ea6b2b74b097279aa5a975af1d1637203
StephanRammos <techeagle289@gmail.com>
2025-07-01 16:20:25 -0700

    initial

---
+++ ch5/5-04.py
@@ -0,0 +1,8 @@
+#5-04.py
+alien_color = 'red'
+if alien_color == 'green':
+	print("You've earned 5 points for shooting this green alien")
+
+else:
+	print("You've earned 10 points for shooting this non-green alien")
+
