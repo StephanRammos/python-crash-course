f6419cfe0fc0791f21dcd7516b741047fc4c8b9e
StephanRammos <techeagle289@gmail.com>
2025-06-30 19:16:27 -0700

    initial

---
+++ ch4/4-09.py
@@ -0,0 +1,6 @@
+#4-09.py
+#Make a list of the cubes of each integer from 1 to 10 inclusive using list comprehension
+
+# list_name = [operation  for i in range( , )]
+cubes = [i**3  for i in range(1,11)]
+print(cubes)
