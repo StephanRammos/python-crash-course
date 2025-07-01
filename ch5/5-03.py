0884bfd278fb6997bf4d41f1a5b82ad295e6e223
StephanRammos <techeagle289@gmail.com>
2025-07-01 16:13:12 -0700

    initial

---
+++ ch5/5-03.py
@@ -0,0 +1,11 @@
+#5-03.py
+#version that passes, output printed
+# alien_color = 'green'
+# if alien_color == 'green':
+# 	print("You just earned 5 points")
+
+#version that fails if test, no output printed
+alien_color ='yellow'
+if alien_color == 'green':
+	print("You just earned 5 points")
+
