4a78471f00790ee578f36336fcd7655f62a14055
StephanRammos <techeagle289@gmail.com>
2025-07-01 18:09:15 -0700

    initial

---
+++ ch5/5-07.py
@@ -0,0 +1,9 @@
+#5-07.py
+favorite_fruits = ['apples', 'bananas', 'watermelon']
+if 'bananas' in favorite_fruits:
+	print("Wow you really like bananas") 
+if 'apples' in favorite_fruits :
+	print("An apple a day , keeps the Dr away")
+if 'oranges' in favorite_fruits:
+	print("Wow you really like oranges")
+	
