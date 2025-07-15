9ce1c046c38a2cdf3e4d3c6e10d0459cfc3f50ec
StephanRammos <techeagle289@gmail.com>
2025-07-14 19:56:26 -0700

    print key,values pairs of a dictionary

--- ch6/6-02.py
+++ ch6/6-02.py
@@ -1,1 +1,17 @@
 #6-02.py
+#print key,values pairs of a dictionary
+fav_nums = {
+	'John': 5,
+	'Mary': 7,
+	'Joseph': 11,
+	'Kira': 22,
+	'Steven': 101
+}
+
+num = fav_nums['John']
+print(f"Mandy's favorite number is {num}\n")
+
+for name, number in fav_nums.items():
+	print(f"{name}'s favorite number is {number}. ")
+
+	
