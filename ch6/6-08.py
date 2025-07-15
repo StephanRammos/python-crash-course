7d353dc09620808443a8d98110bc73bae207f051
StephanRammos <techeagle289@gmail.com>
2025-07-15 15:27:37 -0700

    added comments

--- ch6/6-08.py
+++ ch6/6-08.py
@@ -1,4 +1,6 @@
 #6-08.py
+#append is a list method
+#create dictionaries with pet info and store these dics in a list called pets.
 pets = []
 pet = {'type': 'dog', 'owner':'Gina'}
 pets.append(pet)
@@ -8,4 +10,5 @@ pets.append(pet)
 print(pets)
 
 for pet in pets:
+	#printing elements of each dictionary respectively
 	print(f"This pet is a {pet['type']} and it belongs to {pet['owner']}.")
